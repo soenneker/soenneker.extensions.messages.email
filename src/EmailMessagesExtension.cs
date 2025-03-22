766df153ea9d3d6b46996f1ccb8e0a4cfa87fb64
Jake Soenneker <jake@soenneker.com>
2025-03-21 19:48:03 -0500

    Initial commit

---
+++ src/EmailMessagesExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Soenneker.Extensions.Object;
+using Soenneker.Messages.Email;
+
+namespace Soenneker.Extensions.Messages.Email;
+
+/// <summary>
+/// A collection of helpful EmailMessage extension methods
+/// </summary>
+public static class EmailMessagesExtension
+{
+    /// <summary>
+    /// Converts the properties of an EmailMessage into a dictionary of token strings for use in email templates.
+    /// </summary>
+    /// <param name="message">The EmailMessage to extract tokens from.</param>
+    /// <returns>A dictionary where each key-value pair represents a token and its string value.</returns>
+    public static Dictionary<string, string> ToTokenDictionary(this EmailMessage message)
+    {
+        var tokensToReplace = new Dictionary<string, string>();
+
+        IDictionary<string, object?> propertiesDict = message.ToDictionary();
+
+        foreach (KeyValuePair<string, object?> kvp in propertiesDict)
+        {
+            tokensToReplace.Add(kvp.Key, Convert.ToString(kvp.Value)!);
+        }
+
+        return tokensToReplace;
+    }
+}
---
+++ test/Soenneker.Extensions.Messages.Email.Tests/EmailMessagesExtensionTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Extensions.Messages.Email.Tests;
+
+[Collection("Collection")]
+public class EmailMessagesExtensionTests : FixturedUnitTest
+{
+    public EmailMessagesExtensionTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
